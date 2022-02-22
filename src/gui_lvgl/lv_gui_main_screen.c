9fc039ee1a18cf094069fffbf822c424849dfdea
Denis Davydov <ivnssnn@gmail.com>
2022-02-22 20:14:07 +0300

    feature-added-timer-name-to-main

--- src/gui_lvgl/lv_gui_main_screen.c
+++ src/gui_lvgl/lv_gui_main_screen.c
@@ -11,8 +11,24 @@
 #include "lv_gui_basic.h"
 #include "core/auxiliary.h"
 #include "core/iosettings.h"
+#include "core/auxiliary.h"
+#include "core/rcchannel.h"
 
 static lv_obj_t *lv_main_menu(lv_obj_t *parent, const char *title);
+
+static void st_trim_change_handle(lv_obj_t *obj)
+{
+    int32_t value = lv_slider_get_value(obj);
+    RCChanelSetTrim(value, &RCChanel[0]);
+    settings_changed();
+}
+
+static void th_trim_change_handle(lv_obj_t *obj)
+{
+    int32_t value = lv_slider_get_value(obj);
+    RCChanelSetTrim(value, &RCChanel[1]);
+    settings_changed();
+}
 
 static void servoview_button_handler(lv_event_t *e)
 {
@@ -84,21 +100,6 @@ static lv_obj_t *lv_timer(lv_obj_t *parent, void (*event_cb)(lv_obj_t *obj))
     return label;
 };
 
-static lv_obj_t *lv_slider(lv_obj_t *parent, uint32_t ini_value)
-{
-    lv_obj_t *slider = lv_slider_create(parent);
-    lv_slider_set_mode(slider, LV_SLIDER_MODE_SYMMETRICAL);
-    lv_slider_set_range(slider, GUI_TRIM_MIN, GUI_TRIM_MAX);
-
-    lv_obj_add_style(slider, &bar_bg, LV_PART_MAIN);
-    lv_obj_add_style(slider, &bar_indic, LV_PART_INDICATOR);
-    lv_obj_add_style(slider, &bar_knob, LV_PART_KNOB);
-    lv_obj_set_size(slider, 160, 16);
-    lv_obj_center(slider);
-
-    return slider;
-}
-
 static lv_obj_t *lv_main_menu(lv_obj_t *parent, const char *title)
 {
     const char *lv_menu_buttons_label[] = {"MODEL SELECT",
@@ -134,14 +135,14 @@ lv_obj_t *lv_gui_main_screen(void)
     lv_obj_t *battery_icon = lv_label(screen, LV_TEXT_ALIGN_RIGHT, NULL, LV_SYMBOL_BATTERY_2);
     lv_obj_t *model_name = lv_model_name(screen);
     lv_obj_t *timer1 = lv_timer(screen, set_timer1_data);
+    lv_obj_t *timer1_name = lv_label(screen, LV_TEXT_ALIGN_LEFT, NULL, RCStimerGetName(&RCTimer1));
     lv_obj_t *timer2 = lv_timer(screen, set_timer2_data);
+    lv_obj_t *timer2_name = lv_label(screen, LV_TEXT_ALIGN_LEFT, NULL, RCStimerGetName(&RCTimer2));
+    lv_obj_set_width(timer1_name, 180);
+    lv_obj_set_width(timer2_name, 180);
 
-    lv_obj_t *st_trim = lv_slider(screen, 0);
-    lv_obj_t *th_trim = lv_slider(screen, 0);
-    lv_obj_t *st_name = lv_label(screen, LV_TEXT_ALIGN_RIGHT, NULL, "ST");
-    lv_obj_t *th_name = lv_label(screen, LV_TEXT_ALIGN_RIGHT, NULL, "TH");
-    lv_obj_t *st_value = lv_label(screen, LV_TEXT_ALIGN_LEFT, NULL, "0");
-    lv_obj_t *th_value = lv_label(screen, LV_TEXT_ALIGN_LEFT, NULL, "0");
+    lv_obj_t *st_trim = lv_trim(screen, st_trim_change_handle, RCChanelGetTrim(&RCChanel[0]), "ST");
+    lv_obj_t *th_trim = lv_trim(screen, th_trim_change_handle, RCChanelGetTrim(&RCChanel[1]), "TH");
 
     lv_obj_t *servoview_button = lv_button(screen, servoview_button_handler, "Monitor");
     lv_obj_t *settings_button = lv_button(screen, settings_button_handler, "Settings");
@@ -150,17 +151,14 @@ lv_obj_t *lv_gui_main_screen(void)
     lv_obj_align(battery_icon, LV_ALIGN_TOP_RIGHT, -GUI_MARGIN, 0);
     lv_obj_align_to(battery_label, battery_icon, LV_ALIGN_OUT_LEFT_MID, GUI_MARGIN, 0);
 
-    lv_obj_align_to(model_name, rssi_label, LV_ALIGN_OUT_BOTTOM_LEFT, 16, 10);
-    lv_obj_align_to(timer1, model_name, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
-    lv_obj_align_to(timer2, timer1, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
+    lv_obj_align_to(model_name, rssi_label, LV_ALIGN_OUT_BOTTOM_LEFT, 16, GUI_MARGIN);
+    lv_obj_align_to(timer1_name, model_name, LV_ALIGN_OUT_BOTTOM_LEFT, 0, GUI_MARGIN * 2);
+    lv_obj_align_to(timer1, timer1_name, LV_ALIGN_OUT_BOTTOM_LEFT, 0, GUI_MARGIN);
+    lv_obj_align_to(timer2_name, timer1, LV_ALIGN_OUT_BOTTOM_LEFT, 0, GUI_MARGIN * 4);
+    lv_obj_align_to(timer2, timer2_name, LV_ALIGN_OUT_BOTTOM_LEFT, 0, GUI_MARGIN);
 
-    lv_obj_align(st_trim, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_align_to(st_name, st_trim, LV_ALIGN_OUT_LEFT_MID, -8, 0);
-    lv_obj_align_to(st_value, st_trim, LV_ALIGN_OUT_RIGHT_MID, 8, 0);
-
-    lv_obj_align_to(th_trim, st_trim, LV_ALIGN_OUT_BOTTOM_MID, 0, 16);
-    lv_obj_align_to(th_name, th_trim, LV_ALIGN_OUT_LEFT_MID, -8, 0);
-    lv_obj_align_to(th_value, th_trim, LV_ALIGN_OUT_RIGHT_MID, 8, 0);
+    lv_obj_align(st_trim, LV_ALIGN_CENTER, 0, GUI_MARGIN * 10);
+    lv_obj_align_to(th_trim, st_trim, LV_ALIGN_OUT_BOTTOM_MID, 0, GUI_MARGIN * 2);
 
     lv_obj_align(servoview_button, LV_ALIGN_BOTTOM_RIGHT, -GUI_MARGIN, -GUI_MARGIN);
     lv_obj_align(settings_button, LV_ALIGN_BOTTOM_LEFT, GUI_MARGIN, -GUI_MARGIN);
