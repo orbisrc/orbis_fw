583ff2dfdb94b5fa8677b48c1738732a98df73e8
DENIS DAVYDOV <ivnssnn@gmail.com>
2022-03-13 12:08:43 +0300

    fixes-trim-overflow (#31)

--- src/version.h
+++ src/version.h
@@ -43,7 +43,7 @@
 
 #define FW_VERSION_MAJOR            2  // increment when a major release is made (big new feature, etc)
 #define FW_VERSION_MINOR            2  // increment when a minor release is made (small new feature, change etc)
-#define FW_VERSION_PATCH_LEVEL      1  // increment when a bug is fixed
+#define FW_VERSION_PATCH_LEVEL      2  // increment when a bug is fixed
 
 #define FW_VERSION_STRING STR(FW_VERSION_MAJOR) "." STR(FW_VERSION_MINOR) "." STR(FW_VERSION_PATCH_LEVEL)
 
